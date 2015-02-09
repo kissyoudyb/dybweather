441028c5c71ada16e29136d74e2a7eff460cdc57
kissyoudyb <danyubin1982@163.com>
2015-02-09 13:47:29 +0800

    解决网页无法访问的bug。

--- src/com/dybweather/app/util/HttpUtil.java
+++ src/com/dybweather/app/util/HttpUtil.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import android.util.Log;
+
 public class HttpUtil {
 
 	public static void sendHttpRequest(final String address, final HttpCallbackListener listener){
@@ -21,13 +23,20 @@ public class HttpUtil {
 					connection.setRequestMethod("GET");
 					connection.setConnectTimeout(60000);
 					connection.setReadTimeout(60000);
+					Log.d("HttpUtil", connection.getResponseCode() + " ,responseMessage:" + connection.getResponseMessage());
 					InputStream in = connection.getInputStream();
 					BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 					StringBuilder response = new StringBuilder();
 					String line = "";
-					while((line = reader.readLine()) != null ){
-						response.append(line);
+					
+					while((line = reader.readLine()) != null){
+						if(line.startsWith("<")){
+							throw new Exception("网页无法访问");
+						} else {
+							response.append(line);
+						}
 					}
+					//Log.d("HttpUtil", "result====>: " + response.toString());
 					if(listener != null){
 						//回调onFinish()方法
 						listener.onFinish(response.toString());
