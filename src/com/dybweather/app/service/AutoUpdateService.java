7a3d7342f60383ca7655a3500699403a1fa8a018
kissyoudyb <danyubin1982@163.com>
2015-02-09 13:17:40 +0800

    修改自动更新天气的bug。

--- src/com/dybweather/app/service/AutoUpdateService.java
+++ src/com/dybweather/app/service/AutoUpdateService.java
@@ -1,8 +1,6 @@
 package com.dybweather.app.service;
 
-import com.dybweather.app.util.HttpCallbackListener;
-import com.dybweather.app.util.HttpUtil;
-import com.dybweather.app.util.Utility;
+import java.util.Date;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -13,6 +11,11 @@ import android.os.IBinder;
 import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.util.Log;
+
+import com.dybweather.app.receiver.AutoUpdateReceiver;
+import com.dybweather.app.util.HttpCallbackListener;
+import com.dybweather.app.util.HttpUtil;
+import com.dybweather.app.util.Utility;
 
 public class AutoUpdateService extends Service {
 	
@@ -30,22 +33,23 @@ public class AutoUpdateService extends Service {
 			
 			@Override
 			public void run() {
+				Log.d(TAG, "executed at " + new Date().toString());
 				updateWeather();
 			}
 		}).start();
 		AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
-		int anHour = 60 * 1000; //60Ãë
+		int anHour = 60 * 60 * 1000; //1Ð¡Ê±ºÁÃëÊý
 		long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
-		Intent i = new Intent(this, AutoUpdateService.class);
+		Intent i = new Intent(this, AutoUpdateReceiver.class);
 		PendingIntent pi = PendingIntent.getBroadcast(this, 0, i, 0);
 		manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pi);
 		return super.onStartCommand(intent, flags, startId);
 	}
 	
 	private void updateWeather(){
-		Log.d(TAG, "updateWeather==================>");
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		String weatherCode = prefs.getString("weather_code", "");
+		Log.d(TAG, "updateWeather==================>weatherCode: " + weatherCode);
 		String address = "http://www.weather.com.cn/data/cityinfo/" + weatherCode + ".html";
 		HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
 			
